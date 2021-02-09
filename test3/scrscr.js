7acf625f8a40a36311cc38ef789a9013f820d131
Seungyoon Lee <77139957+syoon0624@users.noreply.github.com>
2021-02-09 10:04:14 +0900

    Add files via upload

---
+++ test3/scrscr.js
@@ -0,0 +1,51 @@
+(function() {
+    'use strict';
+  
+    document.addEventListener('DOMContentLoaded', event => {
+      let connectButton = document.querySelector("#connect");
+      let statusDisplay = document.querySelector('#status');
+      let port;
+  
+      function connect() {
+        port.connect().then(() => {
+          statusDisplay.textContent = 'hello';
+          connectButton.textContent = 'Disconnect';
+          port.onReceive = data => {
+            let textDecoder = new TextDecoder();
+            console.log(textDecoder.decode(data));
+          }
+          port.onReceiveError = error => {
+            console.error(error);
+          };
+        }, error => {
+          statusDisplay.textContent = error;
+        });
+      }
+  
+      connectButton.addEventListener('click', function() {
+        if (port) {
+          port.disconnect();
+          connectButton.textContent = 'Connect';
+          statusDisplay.textContent = '';
+          port = null;
+        } else {
+          serial.requestPort().then(selectedPort => {
+            port = selectedPort;
+            connect();
+          }).catch(error => {
+            statusDisplay.textContent = error;
+          });
+        }
+      });
+  
+      serial.getPorts().then(ports => {
+        if (ports.length == 0) {
+          statusDisplay.textContent = 'No device found.';
+        } else {
+          statusDisplay.textContent = 'Connecting...';
+          port = ports[0];
+          connect();
+        }
+      });
+    });
+  })();
